cf96169f171b00b2a09182e14cb3cd8ed0ad8b3c
BigMao <Apple@AppledeMac-mini.local>
2016-03-28 23:03:57 +0800

    redis版安装入口

--- install3.php
+++ install3.php
@@ -23,7 +23,7 @@ endif;
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>
-	安装Mao10CMS
+	安装Mao10CMS Redis版
 </title>
 <link href="http://cdn.bootcss.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
 <style>
