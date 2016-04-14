2571817f04c89c2707b7c8293f79c19144483a98
BigMao <Apple@AppledeMac-mini.local>
2016-04-14 15:35:39 +0800

    加强了用户资料信息的过滤

--- do/address.php
+++ do/address.php
@@ -1,12 +1,12 @@
 <?php
 require 'functions.php';
 if(maoo_user_id()) :
-	$redis->hset('user:'.maoo_user_id(),'add_name',$_POST['page']['add_name']);
-    $redis->hset('user:'.maoo_user_id(),'add_province',$_POST['province']);
-    $redis->hset('user:'.maoo_user_id(),'add_city',$_POST['city']);
-    $redis->hset('user:'.maoo_user_id(),'add_area',$_POST['area']);
-    $redis->hset('user:'.maoo_user_id(),'add_address',$_POST['page']['add_address']);
-    $redis->hset('user:'.maoo_user_id(),'add_phone',$_POST['page']['add_phone']);
+	$redis->hset('user:'.maoo_user_id(),'add_name',maoo_remove_html($_POST['page']['add_name'],'all'));
+    $redis->hset('user:'.maoo_user_id(),'add_province',maoo_remove_html($_POST['province'],'all'));
+    $redis->hset('user:'.maoo_user_id(),'add_city',maoo_remove_html($_POST['city'],'all'));
+    $redis->hset('user:'.maoo_user_id(),'add_area',maoo_remove_html($_POST['area'],'all'));
+    $redis->hset('user:'.maoo_user_id(),'add_address',maoo_remove_html($_POST['page']['add_address'],'all'));
+    $redis->hset('user:'.maoo_user_id(),'add_phone',maoo_remove_html($_POST['page']['add_phone'],'all'));
 	$url = $redis->get('site_url').'?m=user&a=order&done=默认收货地址设置成功';
 else :
 	$url = $redis->get('site_url').'?m=user&a=login&done=请先登陆';
