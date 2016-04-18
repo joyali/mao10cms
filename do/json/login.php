394758c3954979e53a02f07ecc4e9066b718120c
BigMao <Apple@AppledeMac-mini.local>
2016-04-18 17:54:09 +0800

    增加了手机APP服务端对注册登录功能的支持

--- do/json/login.php
+++ do/json/login.php
@@ -1,34 +1,57 @@
 <?php  
 header('Access-Control-Allow-Origin: *');
 require '../functions.php';
-if($_POST['name'] && $_POST['pass']) :
-	$user_name = $_POST['name'];
-	$user_pass = sha1($_POST['pass']);
-	$id = $redis->zscore('user_id_name',$user_name);
+if($_POST['user_name'] && $_POST['user_pass']) :
+	$user_name = $_POST['user_name'];
+	$user_pass = sha1($_POST['user_pass']);
+	$idname = $redis->zscore('user_id_name',$user_name);
+	$idphone = $redis->zscore('user_id_phone',$user_name);
+	if($idname>0) :
+		$id = $idname;
+	elseif($idphone>0) :
+		$id = $idphone;
+		$user_name = $redis->hget('user:'.$id,'user_name');
+	endif;
 	if($id>0) :
 		$user_pass_true = $redis->hget('user:'.$id,'user_pass');
 		if($user_pass==$user_pass_true) :
 			$date['user_login_date'] = strtotime("now");
+            $date['user_last_ip'] = maoo_user_ip();
+			//积分开始
+			$user_coins_date = $redis->hget('user:'.$id,'user_coins_date')+86400;
+			if($user_coins_date<$date['user_login_date']) :
+				$coins = maoo_user_coins($id);
+				$redis->hset('user:'.$id,'coins',$coins+maoo_coins_every_day());
+				$redis->hset('user:'.$id,'user_coins_date',$date['user_login_date']);
+				$coinsobj->user_id = $id;
+				$coinsobj->des = '登录';
+				$coinsobj->coins = maoo_coins_every_day();
+				$coinsobj->date = strtotime("now");
+				$redis->lpush('coins:user:'.$id,serialize($coinsobj));
+			endif;
+			//积分结束
 			$redis->hmset('user:'.$id,$date);
-			$_SESSION['user_name'] = $user_name;
-			$_SESSION['user_pass'] = $user_pass;
-			$user_level = $redis->hget('user:'.$id,'user_level');
-			echo '{"logindone":true,"id":'.$id.',"error":"登录成功"}';
+            //json
+            $now = $date['user_login_date'];
+            if($redis->hget('user:'.$id,'token_deadline')<($now-86400*3)) :
+                $redis->hset('user:'.$id,'token',maoo_rand(20));
+                $redis->hset('user:'.$id,'token_deadline',$now);
+            endif;
+			$json->code = 200;
+            $json->token = $redis->hget('user:'.$id,'token');
+            $json->des = '登录成功';
+            $json->uid = $id;
 		else :
-			echo '{"logincan":false,"error":"用户名与密码不符"}';
+			$json->code = 502;
+            $json->des = '用户名或密码错误';
 		endif;
 	else :
-		echo '{"logincan":false,"error":"用户名不存在"}';
-	endif;
-elseif($_POST['name']) :
-	$user_name = $_POST['name'];
-	$id = $redis->zscore('user_id_name',$user_name);
-	if($id>0) :
-		echo '{"logincan":true,"error":"用户名验证通过"}';
-	else :
-		echo '{"logincan":false,"error":"用户名不存在"}';
+		$json->code = 502;
+        $json->des = '用户名或密码错误';
 	endif;
 else :
-	echo '{"logincan":false,"error":"用户名和密码必须填写"}';
+	$json->code = 502;
+    $json->des = '用户名或密码未填写';
 endif;
+echo json_encode($json);
 ?>
