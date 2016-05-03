83de144fd7ff72b13b462cd95d9de5bb409461b8
BigMao <Apple@AppledeMac-mini.local>
2016-05-03 10:05:51 +0800

    手机APP用户资料设置模块的支持

--- do/json/user-set.php
+++ do/json/user-set.php
@@ -1,15 +1,17 @@
 <?php  
 header('Access-Control-Allow-Origin: *');
 require '../functions.php';
-if($_GET['uid']>0 && $_GET['token']) :
-    $id = $_GET['uid'];
-	if($_GET['token']==$redis->hget('user:'.$id,'token')) :
+if($_POST['uid']>0 && $_POST['token']) :
+    $id = $_POST['uid'];
+	if($_POST['token']==$redis->hget('user:'.$id,'token')) :
         $now = strtotime("now");
         if($redis->hget('user:'.$id,'token_deadline')<($now-86400*3)) :
             $json->code = 505;
             $json->des = 'token 已过期,请重新登录';
         else :
             $redis->hset('user:'.$id,'token_deadline',$now);
+            $redis->hset('user:'.$id,'display_name',$_POST['display_name']);
+            $redis->hset('user:'.$id,'description',$_POST['description']);
             //json
             $json->code = 200;
             if($redis->hget('user:'.$id,'display_name')) :
